4ed26277abc1abd537c33f88b6dac10d73cd5b2e
edwardsotelojr <edwardsotelo7@gmail.com>
2018-08-06 16:24:10 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
+      resources:posts
+      resources:relationships
+# Define root url
+        root 'pages#index'
+# Define routes for Pages
+          get '/home' => 'pages#home'
+          get '/user/:id' => 'pages#profile'
+          get '/explore' => 'pages#explore'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
