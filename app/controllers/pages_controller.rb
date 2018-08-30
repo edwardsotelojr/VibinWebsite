b0f6b87aab033944da66796b22a4ad2e3a202bef
edwardsotelojr <edwardsotelo7@gmail.com>
2018-08-30 15:58:10 -0500

    Added follow functionality, display count of follows and fllowers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,19 +7,17 @@ class PagesController < ApplicationController
 # back-end code for pages/home
   def home
 @posts = Post.all.where("user_id = ?", current_user.id)
+@followers = Relationship.all.where("followed_id = ?", current_user.id)
   @newpost = Post.new
   end
 # back-end code for pages/profile
   def profile
 
-    if current_user
-        @currentuserfullname = current_user.full_name
-        @currentusername = current_user.username
-    end
     # grab the username from the URL as :id
-    @user = User.all.where("fullname", "test102Full name")
-
+    @user = User.find_by_username params[:id]
+ @followers = Relationship.all.where("followed_id = ?", User.find_by_username(params["id"]).id)
 @posts = Post.all.where("user_id = ?", current_user.id)
+@follow = Relationship.all.where("follower_id = ?", User.find_by_username(params["id"]).id)
 end
 def profileMusic
 
@@ -51,6 +49,9 @@ end
 
 
   private
+    def user_params
+      params.require(:user).permit(:id, :full_name, :email)
+    end
 
     def post_params
         params.require(:post).permit(:user_id, :content)
--- db/migrate/20180629192500_create_posts.rb
+++ db/migrate/20180629192500_create_posts.rb
@@ -1,10 +1,10 @@
-for class CreatePosts < ActiveRecord::Migration[5.2]
+class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.references :user, index: true, foreign_key: true
       t.text :content
       t.timestamps
     end
-      add_index :posts, [:user_id, :created_at]
+    add_index :posts, [:user_id, :created_at]
   end
 end
